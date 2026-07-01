b8aac0756017c81ca48ba141c87ea845b3f3ce26
Favour Egwele <favoregwele@gmail.com>
2026-07-01 16:28:36 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <h1>Hello! Welcome to construction</h1>
+    </main>
+  )
+}
+
+export default App
