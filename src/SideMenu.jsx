92024b079f19a928f3d7b5e78eb042e9f2f99d2a
Favour Egwele <favoregwele@gmail.com>
2026-07-09 15:26:03 +0200

    Setting up side menu

--- src/SideMenu.jsx
+++ src/SideMenu.jsx
@@ -1,7 +1,36 @@
 function SideMenu() {
     return ( 
         <nav>
-            <h1>SIDE MENU!</h1>
+            <section className="logo-sect">
+                <i class="bi bi-check2-circle"></i>
+                <p>Optima</p>
+            </section>
+
+            <section className="nav-sect">
+                
+                <div className="navigation">
+                    <i class="bi bi-house-door-fill"></i>
+                    <p>Home</p>
+                </div>
+
+                <div className="navigation">
+                    <i class="bi bi-bar-chart"></i>
+                    <p>Stats</p>
+                </div>
+                
+                <div className="navigation">
+                    <i class="bi bi-clock-history"></i>
+                    <p>History</p>
+                </div>
+                
+                <div className="navigation">
+                    <i class="bi bi-gear"></i>
+                    <p>Settings</p>
+                </div>
+            
+            </section>
+            
+            <section></section>
         </nav>
     );
 }
