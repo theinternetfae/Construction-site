4a8e877f8b5650f0c08481cf6ab1b63e948b5d56
Favour Egwele <favoregwele@gmail.com>
2026-07-14 11:17:30 +0200

    Finished coding alert box

--- src/utilities jsx/Alert.jsx
+++ src/utilities jsx/Alert.jsx
@@ -3,7 +3,10 @@ import { createPortal } from "react-dom";
 function Alert() {
     return createPortal( 
         <div className="alert-container">
-            ALERTTTTTTTTT
+            <div className="alert-box">
+                <p>Press the button to verify your account</p>
+                <button>Verify</button>
+            </div>
         </div>,
         document.getElementById("modal-root")
     );
