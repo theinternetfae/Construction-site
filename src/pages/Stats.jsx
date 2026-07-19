9dfed59e590659d1141f8e05055dd53ae1f599b7
Favour Egwele <favoregwele@gmail.com>
2026-07-19 22:11:26 +0200

    Stats header

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,8 +1,28 @@
 function Stats() {
     return ( 
-        <>
-            STATS!
-        </>
+        <div className="stats">
+            
+            <section className="stats-header">
+
+                <i className="bi bi-lightbulb-fill"></i>
+                <select name="" id="">
+                    <option value="">📈 Overall</option>
+                    <option value="">✏ Draw</option>
+                    <option value="">💻 SMM</option>
+                    <option value="">🙏🏾 Pray</option>
+                    <option value="">👩🏾‍💻 Code</option>
+                    <option value="">🤸🏾‍♀️ Gym</option>
+                    <option value="">📞 Call mummy</option>
+                </select>
+                <i className="bi bi-clipboard-plus-fill"></i>
+
+            </section>
+            
+            <section>BODY 1</section>
+            
+            <section>BODY 2</section>
+        
+        </div>
     );
 }
 
