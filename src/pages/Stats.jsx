a85c2ebeaaf84716f7cab8188d5cb211c9e39ba1
Favour Egwele <favoregwele@gmail.com>
2026-07-20 13:39:18 +0200

    Stats task list

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -18,9 +18,21 @@ function Stats() {
 
             </section>
             
-            <section>BODY 1</section>
+            <section className="stats-tasks-cont">
+            
+                <p className="stats-task">📈 <span className="stats-task-title block">Overall</span></p>
+                <p className="stats-task">✏ <span className="stats-task-title">Draw</span></p>
+                <p className="stats-task">💻 <span className="stats-task-title">SMM</span></p>
+                <p className="stats-task">🙏🏾 <span className="stats-task-title">Pray</span></p>
+                <p className="stats-task">👩🏾‍💻 <span className="stats-task-title">Code</span></p>
+                <p className="stats-task">🤸🏾‍♀️ <span className="stats-task-title">Gym</span></p>
+                <p className="stats-task">📞 <span className="stats-task-title">Call mummy</span></p>
+            
+            </section>
             
             <section>BODY 2</section>
+
+            <section>BODY 3</section>
         
         </div>
     );
