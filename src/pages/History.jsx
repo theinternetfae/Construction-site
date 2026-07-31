d7e7e09f6af33f55d87d2f7cd937cbf72eb123d5
Favour Egwele <favoregwele@gmail.com>
2026-07-31 23:18:58 +0200

    Finished design for history page

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,8 +1,50 @@
 function History() {
     return ( 
-        <>
-            HISTORY
-        </>
+        <div className="history">
+            <section className="history-finder">
+                <div className="input-date">
+                    <input type="number" placeholder="MM"/>
+                    <input type="number" placeholder="DD"/>
+                    <input type="number" placeholder="YYYY"/>
+                    <i className="bi bi-search"></i>
+                </div>
+                
+                <p className="quick-info">JULY 21, 2026 (0 TASKS)</p>
+            </section>
+
+            <section className="info-on-date">
+                
+                <div className="history-tasks">
+                    SECTION 1
+                </div>
+                
+                <div className="history-result">
+                    <div className="result-cont">
+                        <i className="bi bi-stars"></i>
+                        <p className="result-counter">0 days</p>
+                        <p className="title">Top streak</p>
+                    </div>
+                    
+                    <div className="result-cont">
+                        <i className="bi bi-fire"></i>
+                        <p className="result-counter">0 days</p>
+                        <p className="title">Current streak</p>
+                    </div>
+
+                    <div className="result-cont">
+                        <i className="bi bi-check-circle"></i>
+                        <p className="result-counter">0</p>
+                        <p className="title">Top streak</p>
+                    </div>
+                    
+                    <div className="result-cont">
+                        <i className="bi bi-info-circle"></i>
+                        <p className="result-counter">0 Active</p>
+                        <p className="title">Status</p>
+                    </div>
+                </div>
+            </section>
+        </div>
     );
 }
 
