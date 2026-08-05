1c6d8bdf6c82b33a3fda2e71273416ccd22497f0
Favour Egwele <favoregwele@gmail.com>
2026-08-05 13:35:19 +0200

    Designed profile

--- src/settings pages/Profile.jsx
+++ src/settings pages/Profile.jsx
@@ -1,7 +1,89 @@
 function Profile() {
     return (  
-        <div>
-            PROFILE
+        <div className="profile">
+            
+            <section className="user-display">
+
+                <div className="pfp-cont">
+                    <i class="bi bi-plus-lg"></i>
+                </div>
+            
+                <div className="name-email">
+                    <span>Jane Doe</span>
+                    <span>janedoe@gmail.com</span>
+                </div>
+            
+            </section>
+
+            <div className="demacator"></div>
+            
+            <section className="user-preferences">
+
+                <div className="user-pref">
+                    <p>Themes</p>
+
+                    <div className="theme-array">
+                        <div>
+                            <span className="blue"></span>
+                        </div>
+
+                        <div>
+                            <span className="purple"></span>
+                        </div>
+                        
+                        <div>
+                            <span className="pink"></span>
+                        </div>
+
+                        <div>
+                            <span className="green"></span>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="user-pref">
+                    <p>Optima quirk</p>
+                    <div className="pref-toggle">
+                        <div className="pref-toggle-switch"></div>
+                    </div>
+                </div>
+                
+                <div className="user-pref">
+                    <p>Daily quote</p>
+                    <div className="pref-toggle">
+                        <div className="pref-toggle-switch"></div>
+                    </div>
+                </div>
+                
+                <div className="user-pref">
+                    <p>Streak</p>
+                    <div className="pref-toggle">
+                        <div className="pref-toggle-switch"></div>
+                    </div>
+                </div>
+            
+            </section>
+
+            <div className="demacator"></div>
+
+            <section className="account-commands">
+                
+                <div className="command">
+                    <p>Log out</p>
+                    <button className="log-out">
+                        <i className="bi bi-person-walking"></i>
+                    </button>
+                </div>
+
+                <div className="command">
+                    <p>Delete account</p>
+                    <button className="delete-account">
+                        <i className="bi bi-person-x-fill"></i>
+                    </button>
+                </div>
+            
+            </section>
+        
         </div>
     );
 }
