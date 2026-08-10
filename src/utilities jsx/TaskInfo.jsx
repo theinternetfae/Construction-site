3aa9f51e046036fd6047819cd73831faf157d54d
Favour Egwele <favoregwele@gmail.com>
2026-08-10 12:33:23 +0200

    Setting up taskInfo

--- src/utilities jsx/TaskInfo.jsx
+++ src/utilities jsx/TaskInfo.jsx
@@ -1,6 +1,8 @@
 import { createPortal } from "react-dom";
+import dayjs from "../js files/DayJs.js";
 
 function TaskInfo({task, exit}) {
+    
     return createPortal( 
         <div className="task-info">
 
@@ -9,7 +11,46 @@ function TaskInfo({task, exit}) {
                 borderColor: task.color
             }}>
                 <i className="bi bi-x-square-fill exit-editor" onClick={exit}></i>
-                {task.name}
+
+                <div className="initial-info">
+
+                    <div className="emoji">
+                        {task.emoji}
+                    </div>
+
+                    <div className="tags">
+                        <span>
+                            Name: {task.name}
+                        </span>
+                        
+                        <span>
+                            Color: {task.color === 'var(--accent)' ? 'None' : task.color}                            
+                        </span>
+                    </div>
+
+                </div>
+
+                <div className="further-info">
+
+                    <div className="f-info">Created: <span>{dayjs(task.createdAt).format('MMMM D, YYYY')}</span> </div>
+
+                    <div className="f-info">Scheduled days: <span>{task.days.length !== 0 ? task.days.join(", ") : 'None'}</span></ div>
+
+                    {/* "bi bi-dash-lg" */}
+                    <div className="f-info">Time span: <span>
+                        {dayjs(task.startDate).format('MMMM D, YYYY')}
+                        <i class="bi bi-arrow-right" style={{color: task.color}}></i>
+                        {dayjs(task.endDate).format('MMMM D, YYYY')}</span>
+                    </div>
+
+                    <div className="f-info">Reminder time:<span>{task.reminderTime ? task.reminderTime : 'None'}</span></div>
+
+                    <div className="f-info">Total completed: <span>{task.completed ? "True" : "False"}</span></div>
+
+                    <div className="f-info">Status: <span>Active</span></div>
+
+                </div>
+
             </section>
         
         </div>, document.getElementById("modal-root")
