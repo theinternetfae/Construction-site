c5eeca2da270a473553a33c8a91f4199276710c7
Favour Egwele <favoregwele@gmail.com>
2026-08-12 00:19:40 +0200

    Set up about page functionality

--- src/settings pages/About.jsx
+++ src/settings pages/About.jsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 function About() {
+
+    const [info, setInfo] = useState(false);
+
+
+    const [copied, setCopied] = useState(false);
+    const [copiedText, setCopiedText] = useState('bi-link-45deg');
+
+    function copyText() {
+        navigator.clipboard.writeText("http://localhost:5173/test/home");
+        setCopiedText('bi-check-lg')    
+        setCopied(true);
+    }
+
+    useEffect(() => {
+        if(!copied) return;
+
+        const timer = setTimeout(() => {
+            setCopiedText('bi-link-45deg');
+            setCopied(false);
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, [copied])
+
     return ( 
         <div className="about">
             
             <section className="about-info">
-                <div className="opener">
-                    <i className="bi bi-caret-right"></i>
+                <div className="opener"
+                    onClick={() => setInfo(prev => !prev)}
+                >
+                    <i className={info ? 'bi bi-caret-down' : 'bi bi-caret-right' }></i>
                     What is Optima?
                 </div>
-                <p>Optima is a productivity app that analyzes task completion patterns to dynamically adapt its behavior. Its core feature, a self-evolving system, adjusts based on user consistency to encourage sustainable productivity rather than burnout.</p>
+                <p className={info ? '' : 'hidden'}>Optima is a productivity app that analyzes task completion patterns to dynamically adapt its behavior. Its core feature, a self-evolving system, adjusts based on user consistency to encourage sustainable productivity rather than burnout.</p>
             </section>
         
             <section className="separator">
@@ -23,19 +52,19 @@ function About() {
                         <p>Contact us</p>
                         <p className="subtitle">Send us a message</p>
                     </div>
-                    <button>
+                    <a className="button" href="mailto:favoregwele@gmail.com?subject=Hello">
                         <i className="bi bi-envelope"></i>
-                    </button>
+                    </a>
                 </div>
 
                 <div className="contact">
                     <div className="contact-label">
                         <p>Documentation</p>
-                        <p className="subtitle">Behind the scenes</p>
+                        <p className="subtitle">View the Github code</p>
                     </div>
-                    <button>
+                    <a className="button" href="https://github.com/theinternetfae/Optima-V.2" target="blank">
                         <i className="bi bi-github"></i>
-                    </button>
+                    </a>
                 </div>
 
                 <div className="contact">
@@ -43,8 +72,10 @@ function About() {
                         <p>Share</p>
                         <p className="subtitle">Share to family and friends 🤞</p>
                     </div>
-                    <button>
-                        <i className="bi bi-link-45deg"></i>
+                    <button className="button"
+                        onClick={() => copyText()}
+                    >
+                        <i className={`bi ${copiedText}`}></i>
                     </button>
                 </div>
             
